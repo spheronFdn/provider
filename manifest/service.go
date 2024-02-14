5232ebb679b52f96da5e542100a97304d07bb580
Dusan Stanisavljevic <dusanstanisavljevic@Dusans-MacBook-Pro.local>
2024-02-14 17:43:20 +0100

    Fix 3

--- manifest/service.go
+++ manifest/service.go
@@ -363,7 +363,7 @@ func (s *service) ensureManager(did dtypes.DeploymentID) (manager *manager) {
 func fetchExistingLeases(ctx context.Context, session session.Session) ([]event.LeaseWon, error) {
 	params := &mtypes.QueryLeasesRequest{
 		Filters: mtypes.LeaseFilters{
-			Provider: session.Provider().Address(),
+			Provider: session.Provider().Address().String(),
 			State:    mtypes.LeaseActive.String(),
 		},
 		Pagination: &sdkquery.PageRequest{
