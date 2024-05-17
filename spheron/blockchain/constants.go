3697e4a3360fba807b9becff74d88c5d9cf6069b
Dusan Stanisavljevic <dusanstanisavljevic@Dusans-MacBook-Pro.local>
2024-05-17 15:02:52 +0200

    Update order contract address

--- spheron/blockchain/constants.go
+++ spheron/blockchain/constants.go
@@ -2,7 +2,7 @@ package blockchain
 
 const (
 	providerRegistryContract = "0xEeB0598690fa56Fa89AcAa676190ef2114c61090"
-	orderMatchingContract    = "0x89d2dc132B9f39500A5619f85EF0C8e4a8B63999"
+	orderMatchingContract    = "0x99ABA0c87C0ADDFfa2964eE4a6be5423C55e69eE"
 	tokenRegistryContract    = "0xc2F0490021204B966828cc12ed48e564f095Cf4e"
 )
 
