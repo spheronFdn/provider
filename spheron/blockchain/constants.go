f2859ab20b7927e9033aa140d9582140e5c3fa53
ilij4 <ilijammilosavljevic@gmail.com>
2024-05-21 11:55:59 +0200

    contract

--- spheron/blockchain/constants.go
+++ spheron/blockchain/constants.go
@@ -2,7 +2,7 @@ package blockchain
 
 const (
 	providerRegistryContract = "0xEeB0598690fa56Fa89AcAa676190ef2114c61090"
-	orderMatchingContract    = "0x99ABA0c87C0ADDFfa2964eE4a6be5423C55e69eE"
+	orderMatchingContract    = "0x38FB461B42d6bE1Bc5648A0BC8caE2c74e9D6638"
 	tokenRegistryContract    = "0xc2F0490021204B966828cc12ed48e564f095Cf4e"
 )
 
